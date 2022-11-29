26dc9b1bfa2e4ba0230a164e8ec7d76456542e53
unknown <yousefsaber.155@gmail.com>
2022-11-29 23:03:51 +0200

    fluttertoast

--- bloodify_front_end/lib/modules/login/cubit/states_login.dart
+++ bloodify_front_end/lib/modules/login/cubit/states_login.dart
@@ -4,7 +4,9 @@ class LoginInitialState extends LoginStates {}
 
 class LoginLoadingState extends LoginStates {}
 
-class LoginSuccessState extends LoginStates {}
+class LoginSuccessState extends LoginStates {
+  
+}
 
 class LoginErrorState extends LoginStates {
   final String error;
