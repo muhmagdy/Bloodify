8f098f1d1738ff8b03b61fbaa9539d5a3a5325e3
Youssef <youssefmagdi1210@gmail.com>
2022-12-02 00:08:49 +0200

    added findByNationalID to the DAO

--- backend/src/main/java/com/bloodify/backend/BackendApplication.java
+++ backend/src/main/java/com/bloodify/backend/BackendApplication.java
@@ -66,23 +66,24 @@ public class BackendApplication implements CommandLineRunner {
 
 
 
-//		User guy = new User("Joe", "Biden", "30014120200587",
-//				"Yo@yo.com", "A", '-', false,
-//				LocalDate.now(), "strongPass");
-//
-//		User guy2 = new User("Bla", "BlaBla", "30094120200587",
-//				"as@as.com", "O", '+', false,
-//				LocalDate.now(), "Strrrii");
-//
-//		boolean isInserted = userService.saveUser(guy);
-//		boolean isInserted2 = userService.saveUser(guy2);
-////		String lastName = userService.findUserByEmail("Yo@yo.com").getLastName();
+		User guy = new User("Joe", "Biden", "30014120200587",
+				"Yo@yo.com", "A", '-', false,
+				LocalDate.now(), "strongPass");
+
+		User guy2 = new User("Bla", "BlaBla", "30094120200587",
+				"as@as.com", "O", '+', false,
+				LocalDate.now(), "Strrrii");
+
+		boolean isInserted = userService.saveUser(guy);
+		boolean isInserted2 = userService.saveUser(guy2);
+		String lastName = userService.findUserByNationalID("30094120200587").getLastName();
 //
 ////		boolean signingIn = userService.isUsernameAndPasswordMatching("Yo@yo.com", "strongPass");
 ////		List<User> matchingUsers = userService.getUsersByBloodType("A", '-');
 //
-//		System.out.println("User1 --> " + isInserted + "\nUser2 --> " + isInserted2);
-////				" and lastName is " + lastName + "\nCould Sign in? " + signingIn);
+		System.out.println("User1 --> " + isInserted + "\nUser2 --> " + isInserted2 +
+				" and lastName is " + lastName );
+// + "\nCould Sign in? " + signingIn);
 ////		for(User u : matchingUsers)
 ////			System.out.println(u.getFirstName() + " " + u.getLastName());
 	}
--- backend/src/test/java/com/bloodify/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/bloodify/backend/BackendApplicationTests.java
@@ -1,15 +1,24 @@
 package com.bloodify.backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootTest
 //@ComponentScan
-class BackendApplicationTests {
+class BackendApplicationTests implements CommandLineRunner {
+
+	@Autowired
+	TestUserDAO test;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		test.insertValues();
+	}
 }
