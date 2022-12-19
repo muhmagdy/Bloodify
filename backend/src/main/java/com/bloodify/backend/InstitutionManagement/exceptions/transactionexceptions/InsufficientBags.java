1ca4af10547f6aba3e4d04514fd9bf9353c96904
Youhanna-Yousry <youhanna.yousry.2580@gmail.com>
2022-12-20 00:56:58 +0200

    changed exception message from Insufficient Bags! to Insufficient Blood Bags!

--- backend/src/main/java/com/bloodify/backend/InstitutionManagement/exceptions/transactionexceptions/InsufficientBags.java
+++
@@ -1,7 +0,0 @@
-package com.bloodify.backend.InstitutionManagement.exceptions.transactionexceptions;
-
-public class InsufficientBags extends TransactionException {
-    public InsufficientBags() {
-        super("Insufficient Bags!");
-    }
-}
