a319b5ab748db4884f3dfca7dffdfe1999e908bc
Youhanna-Yousry <youhanna.yousry.2580@gmail.com>
2022-12-21 01:39:27 +0200

    implemented event service

--- backend/src/main/java/com/bloodify/backend/InstitutionManagement/service/classes/EventServiceImp.java
+++ backend/src/main/java/com/bloodify/backend/InstitutionManagement/service/classes/EventServiceImp.java
@@ -1,4 +1,32 @@
 package com.bloodify.backend.InstitutionManagement.service.classes;
 
-public class EventServiceImp {
+import com.bloodify.backend.InstitutionManagement.dto.EventDTO;
+import com.bloodify.backend.InstitutionManagement.dto.mapper.EventDTOMapper;
+import com.bloodify.backend.InstitutionManagement.model.Event;
+import com.bloodify.backend.InstitutionManagement.model.mapper.EventModelMapper;
+import com.bloodify.backend.InstitutionManagement.repository.interfaces.EventDAO;
+import com.bloodify.backend.InstitutionManagement.service.interfaces.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EventServiceImp implements EventService {
+    @Autowired
+    EventDAO eventDAO;
+
+    @Autowired
+    EventModelMapper eventModelMapper;
+
+    @Override
+    public boolean createEvent(EventDTO eventDTO) {
+        return eventDAO.save(eventModelMapper.mapToModel(eventDTO));
+    }
+
+    @Override
+    public List<Event> getAllInstEvents(String email) {
+        return eventDAO.findAllInstEvents(email);
+    }
+
 }
--- backend/src/main/java/com/bloodify/backend/InstitutionManagement/service/interfaces/EventService.java
+++ backend/src/main/java/com/bloodify/backend/InstitutionManagement/service/interfaces/EventService.java
@@ -1,5 +1,12 @@
 package com.bloodify.backend.InstitutionManagement.service.interfaces;
 
+import com.bloodify.backend.InstitutionManagement.dto.EventDTO;
+import com.bloodify.backend.InstitutionManagement.model.Event;
+
+import java.util.List;
+
 public interface EventService {
-    boolean applyUserDonation();
+    boolean createEvent(EventDTO eventDTO);
+
+    List<Event> getAllInstEvents(String email);
 }
