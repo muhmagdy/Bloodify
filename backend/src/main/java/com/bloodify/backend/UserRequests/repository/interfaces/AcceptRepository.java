6a3ab840df43a33f34468f62ef18a124951a381b
Youhanna-Yousry <youhanna.yousry.2580@gmail.com>
2022-12-21 16:25:22 +0200

    added query for updating user info in accepted post

--- backend/src/main/java/com/bloodify/backend/UserRequests/repository/interfaces/AcceptRepository.java
+++ backend/src/main/java/com/bloodify/backend/UserRequests/repository/interfaces/AcceptRepository.java
@@ -4,14 +4,22 @@ import com.bloodify.backend.AccountManagement.model.entities.User;
 import com.bloodify.backend.UserRequests.model.entities.AcceptedPost;
 import com.bloodify.backend.UserRequests.model.entities.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
 public interface AcceptRepository extends JpaRepository<AcceptedPost, Integer> {
-
     List<AcceptedPost> findByPost(Post post);
+
+    @Transactional
+    @Modifying
+    @Query("update AcceptedPost a set a.longitude = ?1, a.latitude = ?2, a.threshold = ?3 where a.user = ?4")
+    void updateLocationInfo(Double longitude, Double latitude, Double threshold, @NonNull User user);
 
     List<AcceptedPost> findByUser(User user);
 }
