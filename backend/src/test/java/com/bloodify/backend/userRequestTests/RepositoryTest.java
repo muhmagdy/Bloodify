ad3528e13321fedae3f30ed5d64612e8d2c24e9d
Muhammad Magdy <muh.magdy1@gmail.com>
2022-12-21 08:49:14 +0200

    fixed some bugs

--- backend/src/test/java/com/bloodify/backend/userRequestTests/RepositoryTest.java
+++ backend/src/test/java/com/bloodify/backend/userRequestTests/RepositoryTest.java
@@ -137,7 +137,7 @@ public class RepositoryTest {
     }
 
     @Test
-    void deleteSpecificPosts(){
+    void deleteSpecificPosts() {
         User user1 = userGenerations.generateRandomUser();
         Institution institution1 = randomizer.generateRandomInstitution();
         User user2 = userGenerations.generateRandomUser();
@@ -145,32 +145,16 @@ public class RepositoryTest {
         Post post1 = new Post(user1, institution1, 1, LocalDateTime.now().minusDays(10), "A+");
         Post post2 = new Post(user2, institution2, 2, LocalDateTime.now(), "A-");
 
-        this.userDAO.saveUser(user1); this.userDAO.saveUser(user2);
-        this.instDao.saveInstitution(institution1); this.instDao.saveInstitution(institution2);
-        this.postRepository.save(post1); this.postRepository.save(post2);
+        this.userDAO.saveUser(user1);
+        this.userDAO.saveUser(user2);
+        this.instDao.saveInstitution(institution1);
+        this.instDao.saveInstitution(institution2);
+        this.postRepository.save(post1);
+        this.postRepository.save(post2);
 
         this.postRepository.deletePostByUserAndInstitutionAndBloodType(user1, institution1, "A+");
 
         assertEquals(this.postRepository.findPostsByUser(user1).size(), 0);
         assertNotNull(this.postRepository.findPostsByUser(user2).get(0));
-    }
-
-    @Test
-    void modifySomePost(){
-        User user1 = userGenerations.generateRandomUser();
-        Institution institution1 = randomizer.generateRandomInstitution();
-        User user2 = userGenerations.generateRandomUser();
-        Institution institution2 = randomizer.generateRandomInstitution();
-        Post post1 = new Post(user1, institution1, 1, LocalDateTime.now().minusDays(10), LocalDateTime.now().plusDays(2), "A+");
-        Post post2 = new Post(user2, institution2, 2, LocalDateTime.now(), LocalDateTime.now().plusDays(2), "A-");
-
-        this.userDAO.saveUser(user1); this.userDAO.saveUser(user2);
-        this.instDao.saveInstitution(institution1); this.instDao.saveInstitution(institution2);
-        this.postRepository.save(post1); this.postRepository.save(post2);
-        this.postRepository.updatePostSet(institution2.getInstitutionID(), 5, "O-", post1.getPostID(),
-                LocalDateTime.now(), LocalDateTime.now().plusDays(3));
-        post1 = this.postRepository.findPostByUserAndInstitutionAndBloodType(user1, institution2, "O-");
-        assertNotNull(post1);
-        assertEquals(post1.getBloodType(), "O-");
     }
 }
