4bb1c7988fe0ad9c01c2d9eb6ed526929644315b
YoussefMagdy10 <youssefmagdi1210@gmail.com>
2022-12-26 23:37:37 +0200

    done: service of institution bags count statistics

---
+++ backend/src/main/java/com/bloodify/backend/Statistics/Service/Institution/BloodBagsCountWrapper.java
@@ -0,0 +1,12 @@
+package com.bloodify.backend.Statistics.Service.Institution;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class BloodBagsCountWrapper {
+    String bloodType;
+    int bagsCount;
+    double bagsPercentage;
+}
