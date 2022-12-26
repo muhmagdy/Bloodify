2beeec474ee4ceca3e59aa802f85dcb2a690c379
Karim Alaa <karimalaa1912@gmail.com>
2022-12-27 01:12:49 +0200

    added service test

--- backend/src/test/java/com/bloodify/backend/Chat/service/classes/ChatServiceImpTest.java
+++ backend/src/test/java/com/bloodify/backend/Chat/service/classes/ChatServiceImpTest.java
@@ -1,12 +1,20 @@
 package com.bloodify.backend.Chat.service.classes;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import com.bloodify.backend.AccountManagement.model.entities.User;
 import com.bloodify.backend.Chat.controller.requests.entities.ChatMessageRequest;
 import com.bloodify.backend.Chat.controller.requests.entities.ChatRequest;
 import com.bloodify.backend.Chat.dto.entities.ChatDto;
@@ -30,40 +38,75 @@ public class ChatServiceImpTest {
 
     ChatService service;
 
+    ChatMessageDto chatMessageDto;
+    List<ChatMessage> msgs;
+    List<ChatMessageDto> msgsDto;
+
+
+    ChatMessage mockMsg(int i){
+        ChatMessage msg  = Mockito.mock(ChatMessage.class);
+        when(msg.getMessageID()).thenReturn(i);
+        when(msg.getContent()).thenReturn(String.valueOf(i*234));
+        return msg;
+    }
+    ChatMessageDto mockMsgDto(int i){
+        ChatMessageDto msg  = Mockito.mock(ChatMessageDto.class);
+        when(msg.getMessageID()).thenReturn(i);
+        when(msg.getContent()).thenReturn(String.valueOf(i*234));
+        return msg;
+    }
+
     @BeforeEach
     void init(){
         msgDao = Mockito.mock(ChatMessageDao.class);
         chatDao = Mockito.mock(ChatDao.class);
         chatMapper = Mockito.mock(ChatMapper.class);
         msgMapper = Mockito.mock(ChatMessageMapper.class);
+        msgs = new LinkedList<>();
+        msgsDto = new LinkedList<>();
+
+
+        for(int i = 0; i < 5; i++){
+            msgs.add(mockMsg(i));
+            msgsDto.add(mockMsgDto(i));
+        }
+        chatMessageDto =  Mockito.mock(ChatMessageDto.class);
         service = new ChatServiceImp(chatDao, msgDao, chatMapper, msgMapper);
     }
     
     @Test
-    void testLoadChatMessages() throws Exception {
-        ChatDto chatDto = new ChatDto(0, 0, 0);
-        when(chatMapper.dtoToEntity(chatDto)).thenThrow(new PostNotFoundException());
+    void testLoadChatMessages_happy() throws Exception {
+        when(msgDao.findChatMessages(3, 5)).thenReturn(msgs);
+        Iterator<ChatMessage> msgIter = msgs.iterator();
+        Iterator<ChatMessageDto> dtoIter = msgsDto.iterator();
+        while(msgIter.hasNext() && dtoIter.hasNext()){
+            when(msgMapper.entityToDto(msgIter.next())).thenReturn(dtoIter.next());
+        }
+        List<ChatMessageDto> actual = assertDoesNotThrow(() -> service.loadChatMessages(3, 5));
 
-        assertDoesNotThrow(() -> service.saveChat(chatDto));
+        assertEquals(msgsDto, actual);
     }
 
     @Test
-    void testLoadChats() {
+    void testLoadChatMessages_noMessages() throws Exception {
+        when(msgDao.findChatMessages(3, 5)).thenReturn(List.of());
+        
 
+        List<ChatMessageDto> actual = assertDoesNotThrow(() -> service.loadChatMessages(3, 5));
+
+        assertEquals(new LinkedList<ChatMessageDto>(), actual);
     }
 
     @Test
-    void testLoadChats2() {
+    void testSaveMessage_happy() throws Exception {
+        ChatMessage msg = Mockito.spy(ChatMessage.class);
 
-    }
+        when(msgMapper.dtoToEntity(chatMessageDto)).thenReturn(msg);
 
-    @Test
-    void testSaveChat() {
+        //TODO
 
-    }
+        // boolean actual = assertDoesNotThrow(() -> service.saveMessage(chatMessageDto));
 
-    @Test
-    void testSaveMessage() {
-
+        // assertTrue(actual);
     }
 }
